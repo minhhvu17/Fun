8babe444ad447563b3a71765865ac2f47ecb2f8d
unknown <you@example.com>
2022-10-20 23:36:48 +0700

    a

---
+++ script.js
@@ -0,0 +1,49 @@
+let Box = document.getElementById("box");
+let ChangePosition2 = document.getElementById("btn2");
+let ChangePosition1 = document.getElementById("btn1");
+let i = 0;
+
+ChangePosition2.addEventListener('mouseover', () => {
+    if (i < 10) {
+        i++;
+        let vertical = Math.floor(Math.random() * 71) + 15;
+        let horizon = Math.floor(Math.random() * (83 - 27 + 1)) + 27;
+        Box.style.left = horizon + "%";
+        Box.style.top = vertical + "%";
+        console.log(i)
+    }
+})
+
+
+ChangePosition1.addEventListener('mouseover', () => {
+    if (i % 2 == 1 && i >= 10) {
+        i++;
+        middle = ChangePosition2.innerHTML;
+        ChangePosition2.innerHTML = ChangePosition1.innerHTML;
+        ChangePosition1.innerHTML = middle;
+        console.log(i)
+    }
+})
+ChangePosition2.addEventListener('mouseover', ()=>{
+    if (i%2 == 0 && i >= 10){
+        i++;
+        middle = ChangePosition2.innerHTML;
+        ChangePosition2.innerHTML = ChangePosition1.innerHTML;
+        ChangePosition1.innerHTML = middle;
+        
+    }
+})
+i++;
+
+
+// if (i > 15){
+//     ChangePosition2.addEventListener('mouseenter', () =>{
+//     ChangePosition2.innerHTML = "";
+// });
+// ChangePosition2.addEventListener('mouseout', () =>{
+//     ChangePosition2.innerHTML = "No";
+// });
+// }
+
+
+
